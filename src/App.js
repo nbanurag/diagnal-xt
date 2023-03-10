c220b87ac5cca0d53dde7711f47a94152956f018
Anurag Bansal <Anurag.Bansal@idc.bedbath.com>
2023-03-10 22:55:00 +0530

    movies listing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+const NavBar = lazy(() => import("./components/NavBar"));
+const Home = lazy(() => import("./components/Home"));
+const Movies = lazy(() => import("./components/Movies"));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+          </Routes>
+        </div>
+      </Suspense>
+    </Router>
   );
-}
+};
 
 export default App;
